815c757250de3388ea24e86e28ba79f3fd1f8a20
dangotsukimi <dango_tsukimi@yahoo.co.jp>
2023-04-19 20:41:53 +0900

    login処理、register処理を追加

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, redirect, abort, url_for, render_template, make_response, session
 from markupsafe import escape
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, query
+from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'cDe#4RfV'
@@ -17,6 +19,23 @@ engine = create_engine("mysql+mysqlconnector://", connect_args=sql_user, echo=Tr
 
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(20), unique=True, nullable=False)
+    password_hash = Column(String(128), nullable=False)
+
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+Base.metadata.create_all(bind=engine)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -30,15 +49,50 @@ def get_test():
     get_word = request.args.get('name', '1')
     return f"Hello, {get_word}"
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    message = None
+    if request.method == 'POST':
+        username = escape(request.form['username'])
+        password = request.form['password']
+        if not username:
+            error = 'Enter username.'
+        elif db_session.query(User).filter_by(username=username).first() is not None:
+            error = 'The username is already registered.'
+
+        else :
+            new_user = User(username=username, password=password)
+            db_session.add(new_user)
+            db_session.commit()
+            message = 'User has been registered.'
+            return render_template('register.html', error=error, message=message)
+    return render_template('register.html', error=error, message=message)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         username = escape(request.form['username'])
-        if username != 'admin':
-            return redirect(url_for('test', username=username))
+        password = request.form['password']
 
-        else :
+        if not username or not password:
+            error = 'Enter both username and password.'
+
+        elif not username.isalnum():
+            error = 'Username can only alphabet or number characters.'
+
+        elif username != 'admin':
+            user = db_session.query(User).filter_by(username=username).first()
+            print(user.password_hash)
+            if not user or not user.check_password(password=password):
+                error = 'Wrong username or password.'
+
+            else:
+                return redirect(url_for('test', username=username))
+
+        else:
             error='Admin is not allowed.'
 
     return render_template('login_form.html', error=error)
