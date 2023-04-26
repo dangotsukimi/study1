01efc0508d77cd6400c5a59445d50b384e1c8429
dangotsukimi <dango_tsukimi@yahoo.co.jp>
2023-04-26 18:02:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,32 +2,22 @@ import secrets
 import re
 from flask import Flask, request, redirect, abort, url_for, render_template, make_response, session
 from markupsafe import escape
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import scoped_session, sessionmaker, query
-from sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-sql_user = {
-    'user': 'dbuser',
-    'password': 'P@ssw0rd',
-    'host': 'localhost',
-    'database': 'study1'
-}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://dbuser:P@ssw0rd@localhost/study1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-engine = create_engine("mysql+mysqlconnector://", connect_args=sql_user, echo=True)
+db = SQLAlchemy(app)
 
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(20), unique=True, nullable=False)
-    password_hash = Column(String(128), nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def __init__(self, username=None, password=None):
         self.username = username
@@ -36,26 +26,14 @@ class User(Base):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-Base.metadata.create_all(bind=engine)
-
 def is_valid_password(password):
     pattern = r'^[\w!@#$%&?_]+$'
     return bool(re.match(pattern, password))
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/id/<int:id>')
-def hello(id):
-    return f"Hello, ID:{id}!"
-
-@app.route('/get_test')
-def get_test():
-    get_word = request.args.get('name', '1')
-    return f"Hello, {get_word}"
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -119,10 +97,6 @@ def login():
 
     return render_template('login_form.html', error=error)
 
-@app.route('/redirect_test')
-def redirect_test():
-    return redirect(url_for('get_test', name='test'))
-
 @app.route('/error_test')
 def error_test():
     abort(404)
@@ -143,17 +117,9 @@ def logout():
     return redirect(url_for('login'))
 
 
-
-
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('error.html'), 404
-
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
-
 
 
 if __name__ == '__main__':
